22965f807b80b3b6bb5c8d2bb0ea22ed612fdfb1
Maria helena Salas <mariahelenasalas83@gmail.com>
2025-05-14 14:52:26 -0500

    feat: :tada: initial project
    initial project Backend

---
+++ proyecto-renta/src/test/java/com/proyecto/proyecto_renta/ProyectoRentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyecto_renta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoRentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
