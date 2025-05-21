dd7462ac439fe3b0eaa08f8c9dcf3439f78dbd4e
KarenLore <karenlorenacriscaceres@gmail.com>
2025-05-21 12:55:11 -0500

    feat: :bug: Correction
    Correction the errors

--- proyecto-renta/src/main/java/com/proyecto/proyecto_renta/infrastructure/security/SecurityConfig.java
+++ proyecto-renta/src/main/java/com/proyecto/proyecto_renta/infrastructure/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
-                "http://localhost:8080"
+                "http://localhost:8081"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
